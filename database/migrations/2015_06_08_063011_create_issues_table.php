26fb6062449565f00748f62cf895684ef5be2794
linroex <linroex@coder.tw>
2015-06-08 14:36:01 +0800

    Issues table migration created

---
+++ database/migrations/2015_06_08_063011_create_issues_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIssuesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('issues', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 100);
+			$table->text('content');
+			$table->date('start_data');
+			$table->date('end_date');
+			$table->integer('user_id')->unsigned();
+			$table->timestamps();
+
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('issues');
+	}
+
+}
