aaf299d90cc85e0d587726c9653e9cdb766ebe93
linroex <linroex@coder.tw>
2015-06-08 14:40:19 +0800

    Upload table migration created

---
+++ database/migrations/2015_06_08_063627_create_uploads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUploadsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('uploads', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('issue_id')->unsigned();
+			$table->text('path');
+			$table->timestamps();
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('issue_id')->references('id')->on('issues');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('uploads');
+	}
+
+}
