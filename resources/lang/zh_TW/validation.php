d855e3a3f311f1e570f49597a4939835308ce7ce
linroex <linroex@coder.tw>
2015-07-17 11:32:27 +0800

    add zh_TW lang file

---
+++ resources/lang/zh_TW/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	"confirmed"            => "密碼輸入不相同，請確認。",
+	"email"                => "電子郵件格式不合格，請確認。",
+	"required"             => ":attribute 欄位是必填的。",
+	"unique"               => ":attribute 已經被使用過",
+];
